5cee7b32dd2dc0bb2561ca87fcae5273aa528070
Willard Joseph <31330608+WillardJoseph26@users.noreply.github.com>
2019-04-03 12:21:53 -0400

    Airport Model Built

---
+++ FlightApp/Models/Airport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightApp.Models
+{
+    public class Airport
+    {
+        public int AirportId { get; set; }
+        public string AirportName { get; set; }
+        public string AirportCity { get; set; }
+        public int StateId { get; set; }
+    }
+}

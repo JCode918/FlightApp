18a9109167bbf67e7fade2ab61454843091cb2f8
Willard Joseph <31330608+WillardJoseph26@users.noreply.github.com>
2019-04-03 19:07:35 -0400

    AirlineCarrier Model Added

---
+++ FlightApp/Models/AirlineCarrier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightApp.Models
+{
+    public class AirlineCarrier
+    {
+        public int AirlineCarrierId { get; set; }
+        public string AirlineCarrierName { get; set; }
+    }
+}

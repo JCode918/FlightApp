dba9cf963f990681d8bc3e6de98a218c060ed7f7
Willard Joseph <31330608+WillardJoseph26@users.noreply.github.com>
2019-04-08 16:24:29 -0400

    Airplane Model Created. This may not be needed.

---
+++ FlightApp/Models/Airplane.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightApp.Models
+{
+    public class Airplane
+    {
+        public int AirplaneId { get; set; }
+        public string AirplaneName { get; set; }
+    }
+}

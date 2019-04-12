de3dbc7f27ecbb199eeaaa1d9b713b3961185d07
Willard Joseph <31330608+WillardJoseph26@users.noreply.github.com>
2019-04-12 14:53:58 -0400

    Updating Airport Class Added City Class

--- FlightApp/Models/Airport.cs
+++ FlightApp/Models/Airport.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.UI.WebControls.WebParts;
 
@@ -12,6 +13,10 @@ namespace FlightApp.Models
         public string AirportName { get; set; }
         public string AirportAbbreviation { get; set; }
         public string AirportCity { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }   
+        public int CityId { get; set; }
+        public City City { get; set; }
         public int StateId { get; set; }
         public virtual State State { get; set; }
     }
